6132c8a40fa8e88e55996fb20c883a85783b80a8
Toropon <duck7quick@gmail.com>
2023-10-07 08:58:12 +0900

    修正：MessageContentの各種パラメータ変更

--- app/models/message_content.rb
+++ app/models/message_content.rb
@@ -1,9 +1,10 @@
 class MessageContent < ApplicationRecord
-  # name, sex, mood, person_type, content属性の検証：空文字でない、各指定文字数未満であること
+  # name, sex, mood, person_type, content属性の検証：各指定文字数未満であること
+  # name, sex, contentが空でない事(presence: true)
   validates :name,    presence: true, length: { maximum: 20 }
   validates :sex,     presence: true, length: { maximum: 10 }
-  validates :mood,    presence: true, length: { maximum: 10 }
-  validates :person_type,    presence: true, length: { maximum: 10 }
+  validates :mood,    length: { maximum: 10 }
+  validates :person_type,   length: { maximum: 10 }
   validates :content, presence: true, length: { maximum: 100 } # 本文は100文字まで入力可
 
   # 並べ替え：投稿の新着順(desc)・古い順(asc)
