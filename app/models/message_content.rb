51a42c08dd59811575ab4dbdece5bff98755c71a
Toropon <duck7quick@gmail.com>
2023-10-07 09:06:56 +0900

    追加：nameにカンマが含まれない様にバリデーションをかけた

--- app/models/message_content.rb
+++ app/models/message_content.rb
@@ -7,8 +7,18 @@ class MessageContent < ApplicationRecord
   validates :person_type,   length: { maximum: 10 }
   validates :content, presence: true, length: { maximum: 100 } # 本文は100文字まで入力可
 
+  # オリジナルの検証用メソッド(バリデーション)を追加
+  validate :validate_name_not_including_comma
+
   # 並べ替え：投稿の新着順(desc)・古い順(asc)
   # 投稿の作成時間を基準にしている
   scope :latest, -> { order(created_at: :desc) }
   scope :old, -> { order(created_at: :asc) }
+
+
+  private
+    # nameがnilの場合に例外が発生しないように&.include?としている
+    def validate_name_not_including_comma
+      errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+    end
 end
