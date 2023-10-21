98c5cba1675a80709524a0e223a2365c4abdffdf
Toropon <duck7quick@gmail.com>
2023-10-21 09:24:59 +0900

    追加・修正：初めての方向けの案内ページの追加、editとheaderのボタンのレイアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get "main_pages/:id", to: "main_pages#home"
   get "message_contents/show/:id", to: "message_contents#:id"
   get "message_contents/edit/:id", to: "message_contents#edit#{:id}"
+
+  get "/explanation", to: "message_contents#explanation"
 end
